885fd35cb5ecfe9e8373a1570797c4d49dd9601b
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-09 11:56:23 +0700

    menambahkan animasi pada card ketika warna sama & beda

--- script.js
+++ script.js
@@ -8,41 +8,49 @@ const game = () => {
         color: "red",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "green",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "blue",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "yellow",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "red",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "green",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "blue",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
       {
         color: "yellow",
         isFlipped: false,
         cleared: false,
+        animate: "",
       },
     ],
     get flippedCards() {
@@ -52,24 +60,40 @@ const game = () => {
       return this.cards.filter((card) => !card.cleared);
     },
     async flipCard(card) {
+      // hanya bisa buka 2 kartu
+      if (this.flippedCards.length == 2) return;
+
       // buka atau tutup kartu
       card.isFlipped = !card.isFlipped;
 
       // cek ketika sudah ada 2 kartu terbuka dan cek warna nya
       if ((this.flippedCards.length = 2)) {
         if (this.flippedCards[0].color === this.flippedCards[1].color) {
+          // beri animasi sebelum hilang
+          this.flippedCards.forEach(
+            (card) => (card.animate = "animate__animated animate__rubberBand")
+          );
           await delay();
           this.flippedCards.forEach((card) => (card.cleared = true));
 
           // cek apakah kartu sudah hilang semua
-          if ((this.remainingCards.length = 0)) {
+          if (this.remainingCards.length == 0) {
             alert("You won!");
+            location.reload();
           }
         }
+
+        // beri animasi sebelum card ditutup kembali
+        this.flippedCards.forEach(
+          (card) => (card.animate = "animate__animated animate__shakeX")
+        );
 
         // tutup semua kartu setelah pengecekan
         await delay();
         this.flippedCards.forEach((card) => (card.isFlipped = false));
+
+        // hilangkan class animasi nya
+        this.remainingCards.forEach((card) => (card.animate = ""));
       }
     },
   };
